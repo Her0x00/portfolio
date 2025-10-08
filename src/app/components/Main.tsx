5f51fb8c6fad562d573bbc296dadf31b4f8f169c
Her0x00 <johndoe@example.com>
2025-10-08 07:54:04 +0300

    I have done some changes to main file and made buttons looking better

--- src/app/components/Main.tsx
+++ src/app/components/Main.tsx
@@ -8,10 +8,15 @@ export function MainPage() {
         <div className='flex flex-col'>
           <h1 className='text-6xl'>Hi, i'm Her0x</h1>
         <p className='text-2xl mt-4 mb-4 text-gray-500'>Computer Science Student & Web Developer</p>
-        <p>I'm passionate about creating digital experiences that make <br /> a difference. Currently studying Computer Science and building <br />projects that solve real-world problems.</p>
+        <p className='text-gray-500'>I'm passionate about creating digital experiences that make <br /> a difference. Currently studying Computer Science and building <br />projects that solve real-world problems.</p>
         <div className='flex gap-6 mt-4'>
-          <button className='bg-black text-white'>View My Work</button>
-          <button className='border border-black p-[10px]'>GitHub Profile</button>
+          <div>
+            <button className='border border-black bg-black text-white h-9 px-3 hover:bg-gray-800 rounded-md transition ease-in-out duration-250'>View My Work</button>
+          </div>
+          <div>
+            <button 
+              className='border border-gray-400 text-black h-9 px-3 rounded-md hover:bg-gray-200 transition ease-in-out duration-250'>GitHub Profile</button>
+          </div>
         </div>
         </div>
       </div>
